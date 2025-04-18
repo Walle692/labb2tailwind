8fec6e1c66d804c2acafe122d1c515161ea2b95a
Walle692 <walwim-2@student.ltu.se>
2025-04-18 13:03:30 +0200

    updatesqfoot

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -5,12 +5,12 @@ const Card = ({ data }) => {
         <div className="flex flex-col md:flex-row border rounded-lg overflow-hidden shadow-md">
             <img
                 src={data.Image}
-                alt={`Price: $${SalePrice}`}
+                alt={`Price: $${data.SalePrice}`}
                 className="w-full md:w-1/3 object-cover"
             />
             <div className="p-4 flex flex-col justify-center">
-                <h2 className="text-xl font-bold mb-2">Price: ${SalePrice}</h2>
-                <p className="text-gray-600">Zip Code: {zip_code}, SqFt: {SqFtTotLiving}</p>
+                <h2 className="text-xl font-bold mb-2">Price: ${data.SalePrice}</h2>
+                <p className="text-gray-600">Zip Code: {data.zip_code}, SqFt: {data.SqFtTotLiving}</p>
             </div>
         </div>
     );
